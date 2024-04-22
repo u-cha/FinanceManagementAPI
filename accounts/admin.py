9eb4e467b2fcf300bd26af794313d320dc4b109d
Sergey Udachin <s.udachin@mail.ru>
2024-04-22 16:47:10 +0300

    feat: register Account related models for admin panel

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
+from .models import Account, HistoricAccountBalance
 
 # Register your models here.
+
+
+@admin.register(Account)
+class AccountAdminModel(admin.ModelAdmin):
+    list_filter = (
+        "owner_id",
+        "date_created",
+        "date_modified",
+    )
+    search_fields = (
+        "account_id",
+        "owner_id",
+    )
+    ordering = ("-date_created",)
+
+
+@admin.register(HistoricAccountBalance)
+class HistoricAccountBalanceAdminModel(admin.ModelAdmin):
+    list_filter = (
+        "account_id",
+        "for_date",
+    )
+    ordering = (
+        "account_id",
+        "-for_date",
+    )
