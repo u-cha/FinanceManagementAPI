cbc238f2e997aca62570ad70c108d7ffefc5ec0d
Sergey Udachin <s.udachin@mail.ru>
2024-04-22 17:10:24 +0300

    feat: register models in admin panel

--- customers/admin.py
+++ customers/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Customer
 
 # Register your models here.
+
+
+@admin.register(Customer)
+class CustomerAdminModel(admin.ModelAdmin):
+    list_filter = (
+        "last_name",
+        "birth_date",
+    )
+    search_fields = (
+        "last_name",
+        "birth_date",
+    )
+    ordering = (
+        "last_name",
+        "first_name",
+    )
--- transactions/admin.py
+++ transactions/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from .models import Transaction
 
 # Register your models here.
+
+
+@admin.register(Transaction)
+class TransactionAdminModel(admin.ModelAdmin):
+    list_filter = (
+        "customer_id",
+        "type",
+        "currency_id",
+        "account_id",
+        "created_at",
+        "amount",
+    )
+    search_fields = (
+        "customer_id",
+        "type",
+        "currency_id",
+        "account_id",
+        "created_at",
+        "amount",
+    )
+    ordering = (
+        "customer_id",
+        "-created_at",
+    )
